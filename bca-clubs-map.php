5dc185e03c2fca76702bf8564db138ffa58d647f
Ari Cooper-Davis <asc223@exeter.ac.uk>
2020-10-22 18:31:22 +0100

    Added icons

--- bca-clubs-map.php
+++ bca-clubs-map.php
@@ -1,17 +1,16 @@
 <?php
 /**
- * Plugin Name: BCA Zoom Endpoint
- * Description: Listen for Zoom webhooks and reallocate licenses to upcoming meetings
- * Plugin URI: https://github.com/BritishCavingAssociation/bca-zoom-endpoint
+ * Plugin Name: BCA Clubs Map
+ * Description: Provides a shortcode that displays BCA clubs on a Leaflet map
+ * Plugin URI: https://github.com/BritishCavingAssociation/bca-clubs-map
  * Author: Ari Cooper-Davis
  * Licence: Unlicense
  * Licence URI: https://unlicense.org
  */
 
-// Don't access this file directly
+// Some generic plugin stuff to make life easier
 defined( 'ABSPATH' ) or die();
 
-// Debugging flag
 $v = False;
 
 if ( is_readable( __DIR__ . '/vendor/autoload.php' ) ) {
@@ -19,18 +18,27 @@ if ( is_readable( __DIR__ . '/vendor/autoload.php' ) ) {
 }
 
 function logf( $log ) {
+  /* A simple logger that doesn't depend on external error/log handlers */
 	file_put_contents(__DIR__ . '/log_'.date("j.n.Y").'.log', date("H:i:s").$log."\r\n", FILE_APPEND);
 }
+
+// Register leaflet without fetching it until needed
+function register_leaflet() {
+  wp_register_style('leaflet_style', 'https://unpkg.com/leaflet@1.7.1/dist/leaflet.css');
+  wp_register_script('leaflet_script', 'https://unpkg.com/leaflet@1.7.1/dist/leaflet.js');
+}
+add_action('wp_enqueue_scripts', 'register_leaflet');
 
 // Shortcodes
 
 function display_clubs_map($atts) {
-/*  */
+  /* This is the function called when the shortcode is used */
 
-  // Parse attributes
-  $b = shortcode_atts( array(
-    'disabled' => 0,
-  ), $atts);
+  // Fetch leaflet
+  wp_enqueue_style('leaflet_style');
+  wp_enqueue_script('leaflet_script');
+
+  $raw_data_url = 'https://gitcdn.xyz/repo/BritishCavingAssociation/bca-clubs-map/master/caving_clubs.csv';
 
   return '<p>There should be a map here</p>';
 }
