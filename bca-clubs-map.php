935f1102e3625135f7e25e4d571cfdf8d0ff1d3f
Ari Cooper-Davis <asc223@exeter.ac.uk>
2020-10-22 19:58:43 +0100

    Basic JS leaflet map test

--- bca-clubs-map.php
+++ bca-clubs-map.php
@@ -32,15 +32,38 @@ add_action('wp_enqueue_scripts', 'register_leaflet');
 // Shortcodes
 
 function display_clubs_map($atts) {
-  /* This is the function called when the shortcode is used */
+  /* This is the function called when the shortcode is used; it just registers
+     the leaflet overheads, the leaflet code we've written, and returns a
+     single div */
 
   // Fetch leaflet
   wp_enqueue_style('leaflet_style');
   wp_enqueue_script('leaflet_script');
 
+  // Start buffering output
+  ob_start();
+  echo '<div id="mapid" style="height: 300px;"></div>';
+
   $raw_data_url = 'https://gitcdn.xyz/repo/BritishCavingAssociation/bca-clubs-map/master/caving_clubs.csv';
 
-  return '<p>There should be a map here</p>';
+  ?>
+  <script>
+    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox/streets-v11',
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: 'pk.eyJ1IjoiYXJpY29vcGVyZGF2aXMiLCJhIjoiY2p4YnBvc3Z3MDBodjQydGw3cHNmNWxycSJ9.IuUoWUeuAqVz4bSos8gOqA'
+    }).addTo(mymap);
+  </script>
+  <?php
+
+  // End buffering output and return
+  $output = ob_get_contents();
+  ob_end_clean();
+  return $output;
 }
 add_shortcode('clubs_map', 'display_clubs_map');
 
